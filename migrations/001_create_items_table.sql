d036458d3b7c0378fd8292f6790a11a2fad63a2f
Akhil Krishnan G <akhilrohini@gmail.com>
2026-02-24 23:31:13 +0530

    DML Updates

--- migrations/001_create_items_table.sql
+++ migrations/001_create_items_table.sql
@@ -67,3 +67,42 @@ UPDATE hive SET about = CASE id
   WHEN 12 THEN 'Chess'
   ELSE about
 END;
+
+-- Insert actual employee data
+INSERT INTO hive (name, dept, about, dob, dp_id) VALUES
+('Agansha Baiju', 'Kochi', NULL, '1990-09-14', NULL),
+('Akhil Krishnan G', 'Cherthala', 'Photography, Art, Movies', '1990-05-22', NULL),
+('Ambika M S', 'Thrikkakara', 'Sustainable Living, Travelling', '1990-03-15', NULL),
+('Ananthalakshmi S', 'Kochi', NULL, '1990-12-31', NULL),
+('Ananthasheshan G', 'Kochi', NULL, '1990-06-20', NULL),
+('Anilda Mariya Siby', 'Kochi', NULL, '1990-10-24', NULL),
+('Anirudh M Pai', 'Kochi', NULL, '1990-06-15', NULL),
+('Ansel James Joseph', 'Kochi', NULL, '1990-05-01', NULL),
+('Arun S Nair', 'Kochi', NULL, '1990-05-04', NULL),
+('Athira Ajoy', 'Kochi', NULL, '1990-11-05', NULL),
+('Basil M Jacob', 'Kochi', NULL, '1990-11-21', NULL),
+('Byju Paul', 'Kochi', NULL, '1990-05-13', NULL),
+('Cibi K Shanmukhan', 'Kochi', NULL, '1990-11-29', NULL),
+('Deepamol K Parameswaran', 'Kochi', NULL, '1990-11-23', NULL),
+('Dhanesh Krishna', 'Kochi', NULL, '1990-12-01', NULL),
+('Dinumol R', 'Kochi', NULL, '1990-07-12', NULL),
+('Gopikrishnan R', 'Kochi', NULL, '1990-06-07', NULL),
+('Hamy Asainar', 'Kochi', NULL, '1990-10-16', NULL),
+('Jibin Joy', 'Kochi', NULL, '1990-09-05', NULL),
+('Jinju Raju', 'Kochi', NULL, '1990-03-09', NULL),
+('John J Jacob', 'Kochi', NULL, '1990-07-27', NULL),
+('Krishnamoorthy T S', 'Kochi', NULL, '1990-08-18', NULL),
+('Navaneeth Ps', 'Kochi', NULL, '1990-05-05', NULL),
+('Nikhil P', 'Kochi', NULL, '1990-07-27', NULL),
+('Parvathy H', 'Calicut', 'Avid Reader, Passionate Foodie, Journaling', '1990-02-16', NULL),
+('Rajesh M', 'Kochi', NULL, '1990-11-11', NULL),
+('Ranjith K R', 'Kochi', NULL, '1990-08-11', NULL),
+('Robin Jose', 'Kochi', NULL, '1990-04-17', NULL),
+('Sajeeth S', 'Kochi', NULL, '1990-09-17', NULL),
+('Sheena Narayanan', 'Kochi', NULL, '1990-06-16', NULL),
+('Sherin Susan Georgey', 'Alappuzha', 'Reading, Gardening, Music', '1990-09-10', NULL),
+('Sibin V S', 'Kochi', NULL, '1990-10-19', NULL),
+('Sreelal T L', 'Kochi', NULL, '1990-05-04', NULL),
+('Sunu C John', 'Kochi', NULL, '1990-09-15', NULL),
+('Vishnu Narayanan O', 'Kochi', NULL, '1990-08-02', NULL),
+('Vyshak O', 'Kochi', NULL, '1990-05-01', NULL);
